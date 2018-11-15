c03c506572f3afe3878744626b1a525d291282e2
adriangmemorandum <adriangomez@memorandum.net>
2018-11-15 20:06:04 +0100

    Prueba updater

--- lib/Services/UpdateService.php
+++ lib/Services/UpdateService.php
@@ -75,16 +75,180 @@ class UpdateService
 			return false;
 		}
 
+
+
 		putenv('COMPOSER_HOME=' . __DIR__ . '/../vendor/bin/composer');
 		//Create the commands
 		$input = new ArrayInput(array('command' => 'update'));
 		//Create the application and run it with the commands
 		$application = new Application();
 		$application->setAutoExit(false);
-		if(!$application->doRun($input)) {
+		if(!$application->run($input)) {
 			$this->errors[]=$this->translation->t('{updater:updater:updater_update_error}');
 			return false;
 		}
+
+		$system = new System();
+
+		$dateString = date("YmdHis");
+
+		$sspDir = 'simplesamlphp'.$dateString;
+
+		$configDir = "ssp-config";
+
+		rename('vendor/simplesamlphp/simplesamlphp','./'.$sspDir);
+		rename('vendor','./'.$sspDir.'/vendor');
+
+		if (!file_exists($configDir)) {
+			mkdir($configDir);
+		}
+
+		if (!file_exists($configDir.'/cert')) {
+			mkdir($configDir.'/cert');
+		}
+
+		if (!file_exists($configDir.'/config')) {
+			mkdir($configDir.'/config');
+		}
+
+		if (!file_exists($configDir.'/metadata')) {
+			mkdir($configDir.'/metadata');
+		}
+
+		if (!file_exists($sspDir.'/cache')) {
+			mkdir($sspDir.'/cache');
+		}
+
+		if (!file_exists($sspDir.'/datadir')) {
+			mkdir($sspDir.'/datadir');
+		}
+
+		$apacheUser = exec('grep "User " `find /etc/ -name httpd.conf` | cut -d " " -f 2');
+		$apacheGroup = exec('grep "Group " `find /etc/ -name httpd.conf` | cut -d " " -f 2');
+		$filePermissions = octdec("0664");
+		$folderPermissions = octdec("0775");
+
+		copy($sspDir."/metadata-templates/saml20-idp-hosted.php", $configDir."/metadata/saml20-idp-hosted.php");
+		copy($sspDir."/metadata-templates/saml20-idp-remote.php", $configDir."/metadata/saml20-idp-remote.php");
+		copy($sspDir."/metadata-templates/saml20-sp-remote.php", $configDir."/metadata/saml20-sp-remote.php");
+		copy($sspDir."/metadata-templates/saml20-idp-hosted.php", $configDir."/metadata/saml20-idp-hosted.php");
+		copy($sspDir."/metadata-templates/saml20-idp-remote.php", $configDir."/metadata/saml20-idp-remote.php");
+		copy($sspDir."/config-templates/acl.php", $configDir."/config/acl.php");
+		copy($sspDir."/config-templates/authmemcookie.php", $configDir."/config/authmemcookie.php");
+		copy($sspDir."/config-templates/authsources.php", $configDir."/config/authsources.php");
+		copy($sspDir."/config-templates/config.php", $configDir."/config/config.php");
+		copy($sspDir."/modules/updater/config_template/updater_config.php", $configDir."/config/updater_config.php");
+
+		if (file_exists($sspDir.'/metadata')) {
+			$system->rmRecursive($sspDir.'/metadata');
+		}
+
+		if (file_exists($sspDir.'/cert')) {
+			$system->rmRecursive($sspDir.'/cert');
+		}
+
+		if (file_exists($sspDir.'/config')) {
+			$system->rmRecursive($sspDir.'/config');
+		}
+
+		symlink ("/var/www/idpref-installer-updater/".$configDir."/metadata/" ,$sspDir."/metadata");
+		symlink ("/var/www/idpref-installer-updater/".$configDir."/cert/" ,$sspDir."/cert");
+		//symlink ("../".$configDir."/config/" ,$sspDir."/config");
+		symlink ("/var/www/idpref-installer-updater/".$configDir."/config/" ,$sspDir."/config");
+
+		chmod($configDir."/metadata/saml20-idp-hosted.php", $filePermissions);
+		chmod($configDir."/metadata/saml20-sp-remote.php", $filePermissions);
+
+		//$system->chmodRecursive($sspDir."/modules", $folderPermissions);
+
+		$this->downloadAndWriteConfig($configDir."/config/config.php");
+
+		if (file_exists($sspDir.'/modules/hubandspoke/default-disable')) {
+			rename($sspDir.'/modules/hubandspoke/default-disable',$sspDir.'/modules/hubandspoke/default-enable');
+		}
+
+		if (file_exists($sspDir.'/modules/exampleauth/default-disable')) {
+			unlink($sspDir.'/modules/exampleauth/default-disable');
+		}
+
+		touch($sspDir.'/modules/exampleauth/enable');
+		touch($sspDir.'/modules/sir2skin/default-enable');
+
+		if (file_exists($sspDir.'/modules/sir2skin/default-disable')) {
+			rename($sspDir.'/modules/sir2skin/default-disable',$sspDir.'/modules/sir2skin/default-enable');
+		}
+
+		if (file_exists($sspDir.'/modules/updater/default-disable')) {
+			rename($sspDir.'/modules/updater/default-disable',$sspDir.'/modules/updater/default-enable');
+		}
+
+		chmod($configDir."/config/config.php", $filePermissions);
+		chmod($sspDir."/modules/idpinstaller/lib/makeCert.sh", $folderPermissions);
+
+		if (file_exists($sspDir.'/modules/sir2skin/default.disable')) {
+			rename($sspDir.'/modules/sir2skin/default.disable',$sspDir.'/modules/sir2skin/default-enable');
+		}
+
+		//$system->chmodRecursive($configDir."/cert", $folderPermissions);
+		chown('composer.json', $apacheUser);
+		chgrp('composer.json', $apacheGroup);
+		//$system->chown_r($sspDir, $apacheUser, $apacheGroup);
+		//$system->chown_r($configDir, $apacheUser, $apacheGroup);
+
+		if(file_exists("./simplesamlphp")){
+			unlink("./simplesamlphp");
+		}
+
+		symlink ($sspDir ,"./simplesamlphp");
+
+		if(!chdir('simplesamlphp')){
+			$this->errors[]=$this->translation->t('{updater:updater:updater_update_error}');
+			return false;
+		}
+
+		$input = new ArrayInput(array('command' => 'install'));
+		if(!$application->run($input)) {
+			$this->errors[]=$this->translation->t('{updater:updater:updater_update_error}');
+			return false;
+		}
+		//exec('composer install', $output, $return);
+		//if (!$return) {
+		    //$this->errros[]="No se ha podido actualizar correctamente.";
+		//}
+
+		$input = new ArrayInput(array('command' => 'dump-autoload -a'));
+		if(!$application->run($input)) {
+			$this->errors[]=$this->translation->t('{updater:updater:updater_update_error}');
+			return false;
+		}
+		//exec('composer dump-autoload -a', $output, $return);
+		//if (!$return) {
+		    //$this->errros[]="No se ha podido actualizar correctamente.";
+		//}
+
+		$input = new ArrayInput(array('command' => 'require composer/composer:dev-master'));
+		if(!$application->run($input)) {
+			$this->errors[]=$this->translation->t('{updater:updater:updater_update_error}');
+			return false;
+		}
+
+
+		if(!chdir('../')){
+			$this->errors[]=$this->translation->t('{updater:updater:updater_update_error}');
+			return false;
+		}
+
+		if(!rename('composer.back.json', 'composer.json')){
+			$this->errors[]=$this->translation->t('{updater:updater:updater_update_error}');
+			return false;
+		}
+		//exec('composer require composer/composer:dev-master', $output, $return);
+		//if (!$return) {
+		    //$this->errros[]="No se ha podido actualizar correctamente.";
+		//}
+
+
+
 
 		/*$system = new System();
 		exec('\cp -r ./vendor/simplesamlphp/simplesamlphp/* ./simplesamlphp', $output, $return);
@@ -148,6 +312,22 @@ class UpdateService
 		//shell_exec('composer update');
 		$system->rmRecursive("./vendor");*/
 	}
+
+	 private function downloadAndWriteConfig($configPath)
+    {
+
+		$ch = curl_init();
+
+		curl_setopt($ch, CURLOPT_URL, "http://www.rediris.es/sir2/IdP/install/config.php.txt");
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+		$result = curl_exec($ch);
+		
+		curl_close ($ch);
+
+		file_put_contents($configPath, $result);
+
+    }
 
 	public function getErrors(){
 		return $this->errors;
